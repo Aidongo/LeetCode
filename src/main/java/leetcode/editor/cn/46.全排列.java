2cc30b17c07ab359d4e28380e751af43ba1d90a4
Ruibin Zhu <arbin0829@gmail.com>
2023-08-27 12:51:36 +0800

    Update 46.全排列.java

--- src/main/java/leetcode/editor/cn/46.全排列.java
+++ src/main/java/leetcode/editor/cn/46.全排列.java
@@ -5,9 +5,42 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 class Solution {
+    // 回溯
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    boolean[] used;
+    
     public List<List<Integer>> permute(int[] nums) {
+        if(nums.length == 0)
+            return res;
         
+        used = new boolean[nums.length];
+        permuteHelper(nums);
+        return res;
+
+    }
+
+    private void permuteHelper(int[] nums){
+        if(path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(used[i]){
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            permuteHelper(nums);
+            path.removeLast();
+            used[i] = false;
+        }
     }
 }
 // @lc code=end
